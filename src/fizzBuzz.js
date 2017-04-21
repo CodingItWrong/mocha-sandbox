f35c215e7b717f813d85b75d590ebe4d839f16e8
Josh Justice <jjustice@bignerdranch.com>
2017-04-20 20:30:14 -0500

    Add FizzBuzz

---
+++ src/fizzBuzz.js
@@ -0,0 +1,17 @@
+const fizzBuzz = function(num) {
+  if( divisibleBy(num, 15) ) {
+    return 'FizzBuzz';
+  } else if( divisibleBy(num, 3) ) {
+    return 'Fizz';
+  } else if( divisibleBy(num, 5) ) {
+    return 'Buzz';
+  } else {
+    return `${num}`;
+  }
+};
+
+const divisibleBy = function(dividend, divisor) {
+  return dividend % divisor == 0;
+}
+
+module.exports = fizzBuzz;
