f4cc1a767b82c8f34408991f669eb4ebcf4bd8c4
Josh Justice <jjustice@bignerdranch.com>
2017-12-05 13:51:03 -0500

    Switch to arrow functions

--- test/ToggleTest.js
+++ test/ToggleTest.js
@@ -3,50 +3,50 @@ const {expect} = require('chai');
 const {spy} = require('sinon');
 const Toggle = require('../src/Toggle');
 
-describe('Toggle', function() {
+describe('Toggle', () => {
   let toggle, onHandler, offHandler;
 
-  beforeEach(function() {
+  beforeEach(() => {
     onHandler = spy();
     offHandler = spy();
     toggle = new Toggle(onHandler, offHandler);
   });
 
-  describe('default state', function() {
-    it('is off', function() {
+  describe('default state', () => {
+    it('is off', () => {
       expect(toggle.isOn()).to.be.false;
     });
   });
 
-  describe('toggling once', function() {
-    beforeEach(function() {
+  describe('toggling once', () => {
+    beforeEach(() => {
       toggle.toggle();
     });
 
-    it('is on', function() {
+    it('is on', () => {
       expect(toggle.isOn()).to.be.true;
     });
 
-    it('calls the onHandler', function() {
+    it('calls the onHandler', () => {
       expect(onHandler.calledOnce).to.be.true;
     });
   });
 
-  describe('toggling twice', function() {
-    beforeEach(function() {
+  describe('toggling twice', () => {
+    beforeEach(() => {
       toggle.toggle();
       toggle.toggle();
     });
 
-    it('is off', function() {
+    it('is off', () => {
       expect(toggle.isOn()).to.be.false;
     });
 
-    it('calls the onHandler', function() {
+    it('calls the onHandler', () => {
       expect(onHandler.calledOnce).to.be.true;
     });
 
-    it('calls the offHandler', function() {
+    it('calls the offHandler', () => {
       expect(offHandler.calledOnce).to.be.true;
     });
   });
