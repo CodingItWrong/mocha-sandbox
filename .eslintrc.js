b8148cdbf5333c86af5368e8d19d5a1d10d3c0c4
Josh Justice <jjustice@bignerdranch.com>
2018-05-27 15:53:27 -0400

    Update my styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,25 +4,18 @@ module.exports = {
         "node": true,
     },
     "extends": "eslint:recommended",
+    "parser": "babel-eslint",
     "parserOptions": {
-      "sourceType": "module",
+        "sourceType": "module",
     },
     "rules": {
-        "array-bracket-spacing": [
-            "error",
-            "never"
-        ],
-        "block-spacing": [
-            "error",
-            "always"
-        ],
-        "brace-style": [
-            "error",
-            "1tbs"
-        ],
-        "camelcase": [
-            "error"
-        ],
+        // safety
+        "curly": ["error", "all"],
+        "eqeqeq": ["error", "always"],
+        "no-var": ["error"],
+        "semi": ["error", "always"],
+
+        // diffs
         "comma-dangle": [
             "error",
             {
@@ -30,98 +23,30 @@ module.exports = {
                 "objects": "only-multiline",
             }
         ],
-        "comma-spacing": [
-            "error"
-        ],
-        "comma-style": [
-            "error",
-            "last"
-        ],
-        "curly": [
-            "error",
-            "all"
-        ],
-        "eol-last": [
-            "error",
-            "always"
-        ],
-        "eqeqeq": [
-            "error",
-            "always"
-        ],
-        "func-call-spacing": [
-            "error",
-            "never"
-        ],
-        "indent": [
-            "error",
-            2
-        ],
-        "key-spacing": [
-            "error"
-        ],
-        "keyword-spacing": [
-            "error"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "max-len": [
-            "error",
-            {
-                "code": 80
-            }
-        ],
-        "no-multiple-empty-lines": [
-            "error",
-            {
-                "max": 1
-            }
-        ],
-        "no-trailing-spaces": [
-            "error"
-        ],
-        "no-whitespace-before-property": [
-            "error"
-        ],
-        "object-curly-spacing": [
-            "error",
-            "never"
-        ],
-        "padded-blocks": [
-            "error",
-            "never"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "space-before-blocks": [
-            "error",
-            "always"
-        ],
-        "space-before-function-paren": [
-            "error",
-            "never"
-        ],
-        "space-in-parens": [
-            "error",
-            "never"
-        ],
-        "no-var": [
-            "error"
-        ],
-        "prefer-const": [
-            "error",
-        ],
-        "template-curly-spacing": [
-            "error",
-            "never"
-        ]
+
+        // style
+        "array-bracket-spacing": ["error", "never"],
+        "block-spacing": ["error", "always"],
+        "brace-style": ["error", "1tbs"],
+        "camelcase": ["error"],
+        "comma-spacing": ["error"],
+        "comma-style": ["error", "last"],
+        "eol-last": ["error", "always"],
+        "func-call-spacing": ["error", "never"],
+        "indent": ["error", 2],
+        "key-spacing": ["error"],
+        "keyword-spacing": ["error"],
+        "linebreak-style": ["error", "unix"],
+        "max-len": ["error", { "code": 80 }],
+        "no-multiple-empty-lines": ["error", { "max": 1 }],
+        "no-trailing-spaces": ["error"],
+        "no-whitespace-before-property": ["error"],
+        "object-curly-spacing": ["error", "always"],
+        "padded-blocks": ["error", "never"],
+        "quotes": ["error", "single"],
+        "space-before-blocks": ["error", "always"],
+        "space-before-function-paren": ["error", "never"],
+        "space-in-parens": ["error", "never"],
+        "template-curly-spacing": ["error", "never"],
     }
 };
