2044fbb080bd07b318f8f2c761bb85f22a3a0692
Josh Justice <me@codingitwrong.com>
2018-08-27 20:24:03 -0400

    Switch to private fields

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,20 +1,24 @@
-module.exports = class Toggle {
+export default class Toggle {
+  #on;
+  #onHandler;
+  #offHandler;
+
   constructor(onHandler, offHandler) {
-    this.on = false;
-    this.onHandler = onHandler;
-    this.offHandler = offHandler;
+    this.#on = false;
+    this.#onHandler = onHandler;
+    this.#offHandler = offHandler;
   }
 
   isOn() {
-    return this.on;
+    return this.#on;
   }
 
   toggle() {
-    this.on = !this.on;
-    if (this.on) {
-      this.onHandler();
+    this.#on = !this.#on;
+    if (this.#on) {
+      this.#onHandler();
     } else {
-      this.offHandler();
+      this.#offHandler();
     }
   }
 };
